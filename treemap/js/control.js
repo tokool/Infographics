0e8a5f78005f953fe03aa5d284ed3199bf83fdb9
tokool <tokool@hotmail.co.uk>
2014-07-11 12:03:57 +0100

    Update control.js

--- treemap/js/control.js
+++ treemap/js/control.js
@@ -3,16 +3,16 @@ $( document ).ready(function() {
 	// loadJSONFile('data/portaldata.json');
 
 	// Convert a csv file (tabular data) into a json hierarchical structure
-	d3.csv("data/data_file.csv", function(csv_data){
+	d3.csv("data/Home_Office_Air_Travel_Data_2011.csv", function(csv_data){
 		
 		// Nest function: Converts tabular data into a hierarchy
 		// Add, remove or change the key values to change the hierarchy 
 		// To do this change d.grand_parent, d.parent and d.child to the column titles in the order you wish to nest them, e.g. d.column_1, d.column_2 etc.
 		// NOTE: Your column titles cannot contain spaces.
 		var nested_data = d3.nest()
-			.key(function(d)  { return d.grand_parent; })
-			.key(function(d)  { return d.parent; })
-			.key(function(d)  { return d.child; })
+			.key(function(d)  { return d.Destination; })
+			.key(function(d)  { return d.Supplier_name; })
+			.key(function(d)  { return d.Ticket_class_description; })
 			.entries(csv_data);
 
 		// Creat the root node for the treemap
@@ -23,7 +23,7 @@ $( document ).ready(function() {
 		root.values = nested_data;
 
 		// Change the key names and children values from .next and add values for a chosen column to define the size of the blocks, e.g. a value
-		root = reSortRoot(root,"size_column");
+		root = reSortRoot(root,"Paid_fare");
 
 		// DEBUG
 		// $("#rawdata").html(JSON.stringify(root));
